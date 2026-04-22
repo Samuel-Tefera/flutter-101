e0d3c2442a64ba543f7dd04e90c8609607f2a14e
Samuel-Tefera <samuelteferab@gmail.com>
2026-04-22 10:34:04 +0300

    create start screen for the app

---
+++ quiz_app/lib/start_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset(
+          'assets/images/quiz-logo.png',
+          width: 280,
+          color: const Color.fromARGB(150, 255, 255, 255),
+        ),
+        SizedBox(height: 50),
+        const Text(
+          "Learn flutter in fun way",
+          style: TextStyle(color: Colors.white, fontSize: 24),
+        ),
+        SizedBox(height: 50),
+        OutlinedButton.icon(
+          onPressed: () => {},
+          style: OutlinedButton.styleFrom(
+            foregroundColor: Colors.white,
+            side: BorderSide(color: Color.fromARGB(255, 59, 1, 116)),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(6),
+            ),
+          ),
+          icon: Icon(Icons.arrow_right_alt),
+          label: const Text("Start Quiz"),
+        ),
+      ],
+    );
+  }
+}
