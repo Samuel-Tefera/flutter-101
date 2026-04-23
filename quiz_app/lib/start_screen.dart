8107a1dfd7bd0e904b46f1618e41f3fc9ab68323
Samuel-Tefera <samuelteferab@gmail.com>
2026-04-23 21:26:37 +0300

    make the start quiz button work

--- quiz_app/lib/start_screen.dart
+++ quiz_app/lib/start_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget {
-  const StartScreen({super.key});
+  const StartScreen(this.startQuiz, {super.key});
+
+  final void Function() startQuiz;
 
   @override
   Widget build(context) {
@@ -20,7 +22,7 @@ class StartScreen extends StatelessWidget {
         ),
         SizedBox(height: 50),
         OutlinedButton.icon(
-          onPressed: () => {},
+          onPressed: startQuiz,
           style: OutlinedButton.styleFrom(
             foregroundColor: Colors.white,
             side: BorderSide(color: Color.fromARGB(255, 59, 1, 116)),
